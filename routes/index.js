90b6107be16a727fab187a30c7eb9dbc84d4701e
Manasa Geemarahalli Prakash <manasamanu.gp@gmail.com>
2021-05-07 14:50:32 -0400

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const customers = require('./customers');
 const path = require('path');
 
 const constructorMethod = (app) => {
+    app.use("/", (req, res) => {
+        res.render("layouts/main")
+    })
     app.use('/customers', customers);
     app.use('*', (req, res) => {
         res.status(404).json({error: 'Not found'});
