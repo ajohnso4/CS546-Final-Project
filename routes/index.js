bdedc9131fe4f47ef5040da2751c814bc16f2934
ajohnso4 <ajohnso4@stevens.edu>
2021-05-11 15:19:09 -0400

    added reviews routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const customers = require('./customers');
 const restaurants = require('./restaurants')
 const path = require('path');
+const path = require('./reviews');
 
 const constructorMethod = (app) => {
     app.use('/customers', customers);
     app.use('/restaurants', restaurants);
+    app.use('/reviews', reviews);
     app.get('/', (req, res) => {
         res.render('home/home', {Title: 'Restaurant Table Reservation Page'});
     });
